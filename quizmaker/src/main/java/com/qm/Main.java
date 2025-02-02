4221a607228c21ae321275ef4c0ef9c58c39a093
Joel Sage <joel.sage@student.manchester.ac.uk>
2025-02-02 11:52:46 +0000

    HTTP conncection to send json files & JSON request implemented.

--- quizmaker/src/main/java/com/qm/Main.java
+++ quizmaker/src/main/java/com/qm/Main.java
@@ -1,7 +1,33 @@
 package com.qm;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String model = "deepseek-r1:1.5b";
+        String prompt = "what is deepseek?";
+        String fullResponse = "";
+
+        try{
+            //Set up an HTTP POST request
+            URL url = new URI("http://localhost:11434/api/generate").toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+            
+            // Create request JSON
+            JSONObject requestJson = new JSONObject();
+            requestJson.put("model", model);
+            requestJson.put("prompt", prompt);
+            requestJson.put("stream", false); //gives result when done (instead of word by word)
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
